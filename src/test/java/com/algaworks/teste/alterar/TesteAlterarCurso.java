94db1e47eb2804e4fab2a2642c02c7e31fa4bc91
MaykLucas <mayklucas2015@gmail.com>
2017-10-27 17:28:47 -0200

    criando mais classes de testes

---
+++ src/test/java/com/algaworks/teste/alterar/TesteAlterarCurso.java
@@ -0,0 +1,16 @@
+package com.algaworks.teste.alterar;
+
+import com.algaworks.dao.CursoDAO;
+import com.algaworks.model.Curso;
+
+public class TesteAlterarCurso {
+
+	public static void main(String[] args) throws Exception {
+       CursoDAO dao = new CursoDAO();
+       Curso curso = dao.listarPorId(Curso.class, 3);
+       curso.setNome("Html");
+       curso.setDuracao("34hs");
+       dao.alterar(curso);
+	}
+
+}
--- src/test/java/com/algaworks/teste/inserir/TesteInserirCurso.java
+++ src/test/java/com/algaworks/teste/inserir/TesteInserirCurso.java
@@ -1,9 +1,16 @@
 package com.algaworks.teste.inserir;
 
+import com.algaworks.dao.CursoDAO;
+import com.algaworks.model.Curso;
+
 public class TesteInserirCurso {
 
-	public static void main(String[] args) throws Exception{
-        
+	public static void main(String[] args) {
+		CursoDAO dao = new CursoDAO();
+		Curso curso = new Curso();
+		curso.setNome("SpringBoot");
+		curso.setDuracao("32hs");
+		dao.salvar(curso);
 	}
 
 }
