014e5f92aaedf70b2138cdb4abb3bf18731c202a
MaykLucas <mayklucas2015@gmail.com>
2017-10-27 17:45:24 -0200

    criando a classes de testes do listarPorId

---
+++ src/test/java/com/algaworks/teste/listarPorId/TesteListarPorIdCurso.java
@@ -0,0 +1,16 @@
+package com.algaworks.teste.listarPorId;
+
+import com.algaworks.dao.CursoDAO;
+import com.algaworks.model.Curso;
+
+public class TesteListarPorIdCurso {
+
+	public static void main(String[] args) throws Exception {
+        CursoDAO dao = new CursoDAO();
+        Curso curso = dao.listarPorId(Curso.class, 3);
+        System.out.println(curso.getId());
+        System.out.println(curso.getNome());
+        System.out.println(curso.getDuracao());
+	}
+
+}
